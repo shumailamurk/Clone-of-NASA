33b2c5441d9dca983ee8c849b888400f2030de6a
Dr Shumaila Murk <148242038+shumailamurk@users.noreply.github.com>
2024-09-14 16:25:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function learnmore(){
+    window.open("_blank https://www.nasa.gov/news-release/nasa-sets-coverage-for-starliner-news-conference-return-to-earth/")
+    
+} 
+ function livee(){
+    window.open("https://plus.nasa.gov/")
+ }
+
+ function more1(){
+    window.open("https://www.nasa.gov/gallery/wildlife-birds/")
+ }
+ 
+ function more2(){
+    window.open("https://www.nasa.gov/gallery/wildlife-birds/")
+ }
+ 
+ function more3(){
+    window.open("https://www.nasa.gov/gallery/wildlife-birds/")
+ }
+
+ function repit1(){
+    window.open("https://www.nasa.gov/gallery/wildlife-reptiles/")
+ }
+ function repit2(){
+    window.open("https://www.nasa.gov/gallery/wildlife-reptiles/")
+ }
+ function repit3(){
+    window.open("https://www.nasa.gov/gallery/wildlife-reptiles/")
+ }
+ function animal(){
+    window.open("https://www.nasa.gov/gallery/wildlife-mammals/")
+ }
+
+function explory(){
+    window.open("https://earth.gov/")
+}
+
+function browse(){
+    window.open("https://www.nasa.gov/image-of-the-day/")
+}
+
+function privacy(){
+   window.open("https://www.nasa.gov/privacy/")
+}
+
+function sign(){
+   window.open("https://lp.constantcontactpages.com/su/hHN32CZ/nasagov")
+}
+
+function abouty(){
+   window.open("https://www.nasa.gov/about/")
+}
+
+
+
